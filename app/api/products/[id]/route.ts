4206909b04044fa94fd8c2189d23e9345c6c11b4
pasadem <pasadem@ukr.net>
2025-08-14 16:43:49 +0000

    Initial comm

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -3,7 +3,8 @@ import { Product } from '@/lib/models/Product';
 
 export async function GET(request, { params }) {
   await dbConnect();
-  const product = await Product.findById(params.id);
+  const productId = await params.id;
+  const product = await Product.findById(productId);
   if (!product) {
     return new Response('Not found', { status: 404 });
   }
